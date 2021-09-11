58f9846c565e67dea7fd2d020be1b2b01a2aa339
Solana Cube <90105182+SolanaCube@users.noreply.github.com>
2021-09-11 09:16:28 +0530

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,88 @@
+/** @jsxImportSource theme-ui */
+import type { NextPage } from "next"
+import Head from "next/head"
+import { Container, Flex } from "theme-ui"
+import dynamic from "next/dynamic"
+
+const WalletProviderSection = dynamic(
+  () => import("../components/WalletProviderSection/WalletProviderSection"),
+  {
+    ssr: false,
+  }
+)
+
+const Home: NextPage = () => {
+  return (
+    <div
+      sx={{
+        padding: "0 1.6rem",
+      }}
+    >
+      <Head>
+        <title>Solcube</title>
+        <meta
+          name="description"
+          content="Mint NFTs from a candy machine on Solana blockchain"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Flex
+        sx={{
+          position: "sticky",
+          height: "9rem",
+          alignItems: "center",
+        }}
+      >
+        <Container>
+          <Flex
+            sx={{
+              justifyContent: "space-between",
+              padding: "1.6rem 0",
+            }}
+          >
+            <Flex
+              sx={{
+                verticalAlign: "middle",
+                fontSize: "2.2rem",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "5rem",
+                height: "5rem",
+              }}
+            >
+              <a href="#">
+                <img src="/images/logo.png" />
+              </a>
+            </Flex>
+          </Flex>
+        </Container>
+      </Flex>
+
+      <main>
+        <Container>
+          <header
+            sx={{
+              marginBottom: "6.4rem",
+            }}
+          >
+            <h1>Solcube</h1>
+            <p>
+              Mint your own unique NFT from our limited collecton now! Also,
+              read more about our project <a href="#">here</a>
+            </p>
+          </header>
+          <hr
+            sx={{
+              margin: "1.6rem 0",
+            }}
+          />
+
+          <WalletProviderSection />
+        </Container>
+      </main>
+    </div>
+  )
+}
+
+export default Home
